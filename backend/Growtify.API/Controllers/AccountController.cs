a2370b6444c30a47008365c2aed6680d9fbd5515
Bozhidar1804 <bobo1804@abv.bg>
2025-11-10 01:17:23 +0200

    Implemented AccountControler with POST Register functionality

---
+++ backend/Growtify.API/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Growtify.Domain.Entities;
+using Growtify.Infrastructure.Data;
+
+namespace Growtify.API.Controllers
+{
+    public class AccountController(GrowtifyDbContext dbContext) : BaseApiController
+    {
+        [HttpPost("register")] // POST: api/account/register
+        public async Task<ActionResult<AppUser>> Register(string email, string userName, string password)
+        {
+            using var hmac = new HMACSHA512();
+
+            AppUser newUser = new AppUser
+            {
+                Email = email,
+                UserName = userName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            dbContext.AppUsers.Add(newUser);
+            await dbContext.SaveChangesAsync();
+
+            return newUser;
+        }
+    }
+}
