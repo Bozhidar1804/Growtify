5cf4e3486b3669bc81e55fadb861e06e9e6c59c5
Bozhidar1804 <bobo1804@abv.bg>
2025-11-26 14:51:55 +0200

    Implementing routing

--- frontend/growtify-client/src/app/app.routes.ts
+++ frontend/growtify-client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { MyGrowth } from '../features/my-growth/my-growth';
+import { Community } from '../features/community/community';
+import { UserDetailed } from '../features/account/user-detailed/user-detailed';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'my-growth', component: MyGrowth},
+    {path: 'account/:id', component: UserDetailed},
+    {path: 'community', component: Community},
+    {path: '**', component: Home}
+];
---
+++ frontend/growtify-client/src/features/my-growth/my-growth.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-growth',
+  imports: [],
+  templateUrl: './my-growth.html',
+  styleUrl: './my-growth.css',
+})
+export class MyGrowth {
+
+}
