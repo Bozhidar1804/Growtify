f0e2d841b56a118c4989867b1c0ba460b81d3b8f
Bozhidar1804 <bobo1804@abv.bg>
2026-03-05 03:44:27 +0200

    Adding a modal for filters

--- frontend/growtify-client/src/features/community/community.ts
+++ frontend/growtify-client/src/features/community/community.ts
@@ -1,17 +1,19 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { MemberService } from '../../core/services/member-service';
 import { Member, MemberParams } from '../../types/member';
 import { MemberCard } from "../members/member-card/member-card";
 import { PaginatedResult } from '../../types/pagination';
 import { Paginator } from "../../shared/paginator/paginator";
+import { FilterModal } from '../members/filter-modal/filter-modal';
 
 @Component({
   selector: 'app-community',
-  imports: [MemberCard, Paginator],
+  imports: [MemberCard, Paginator, FilterModal],
   templateUrl: './community.html',
   styleUrl: './community.css',
 })
 export class Community implements OnInit {
+  @ViewChild('filterModal') modal!: FilterModal;
   private memberService = inject(MemberService);
   protected paginatedMembers = signal<PaginatedResult<Member> | null>(null);
   protected memberParams = new MemberParams();
@@ -31,6 +33,23 @@ export class Community implements OnInit {
   onPageChange(event: { pageNumber: number; pageSize: number }) {
     this.memberParams.pageNumber = event.pageNumber;
     this.memberParams.pageSize = event.pageSize;
+    this.loadMembers();
+  }
+
+  openModal() {
+    this.modal.open();
+  }
+
+  onClose() {
+    console.log('Modal closed');
+  }
+
+  onFilterChange(data: MemberParams) {
+    console.log('Modal submitted data: ', data);
+  }
+
+  resetFilters() {
+    this.memberParams = new MemberParams();
     this.loadMembers();
   }
 }
