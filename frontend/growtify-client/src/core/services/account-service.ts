ddfbb8e07567aeda9cee41627ae6067f8329de2e
Bozhidar1804 <bobo1804@abv.bg>
2026-03-06 00:52:29 +0200

    Persisting the filters

--- frontend/growtify-client/src/core/services/account-service.ts
+++ frontend/growtify-client/src/core/services/account-service.ts
@@ -39,7 +39,8 @@ export class AccountService {
   }
 
   logout() {
-    localStorage.removeItem("user");
+    localStorage.removeItem('user');
+    localStorage.removeItem('filters');
     this.currentUser.set(null);
   }
 }
