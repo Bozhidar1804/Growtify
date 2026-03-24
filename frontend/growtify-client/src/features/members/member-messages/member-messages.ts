5923ee16c96243a3a859b4567340b9fddd3f0abd
Bozhidar1804 <bobo1804@abv.bg>
2026-03-24 22:37:15 +0200

    Added scroll to bottom functionality

--- frontend/growtify-client/src/features/members/member-messages/member-messages.ts
+++ frontend/growtify-client/src/features/members/member-messages/member-messages.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { MessageService } from '../../../core/services/message-service';
 import { MemberService } from '../../../core/services/member-service';
 import { Message } from '../../../types/message';
@@ -13,13 +13,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './member-messages.css',
 })
 export class MemberMessages implements OnInit {
+  @ViewChild('messageEndRef') messageEndRef!: ElementRef
   private messageService = inject(MessageService);
   private memberService = inject(MemberService);
   protected messages = signal<Message[]>([]);
   protected messageContent = '';
 
+  constructor() {
+    effect(() => {
+      const currentMessages = this.messages();
+      if (currentMessages.length > 0) {
+        this.scrollToBottom();
+      }
+    })
+  }
+
   ngOnInit(): void {
-      this.loadMessages();
+    this.loadMessages();
   }
 
   loadMessages() {
@@ -29,7 +39,7 @@ export class MemberMessages implements OnInit {
         next: messages => this.messages.set(messages.map(message => ({
           ...message,
           currentUserSender: message.senderId !== memberId
-        })))
+        }))),
       })
     }
   }
@@ -44,6 +54,14 @@ export class MemberMessages implements OnInit {
           return [...messages, message]
         });
         this.messageContent = '';
+      }
+    })
+  }
+
+  scrollToBottom() {
+    setTimeout(() => {
+      if (this.messageEndRef) {
+        this.messageEndRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
       }
     })
   }
